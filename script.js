b4b1ca0f543211b48eefd0e389e9491d27be1443
Jordan Bédard-Lessard <34403496+JordanBedard@users.noreply.github.com>
2023-03-29 22:12:54 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function qui agit sur la Grid 64x64 // 128 x 128 en jouant fonction des paramètres sélectionnés
+
+// Pour la grille - 2 loops - 1 pour la rangée et 1 pour les colonnes
+
+// Function Erase - pourrait être la façon de clearer quand on active les changements de la grid // changement de couleur // ça fait un reset
+
+
+
